e9be98052b6a24f3a6391d05feba6e3e17bd1bc7
Nico Martin <mail@nicomartin.ch>
2022-02-14 10:26:45 +0100

    ssr tests

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -45,7 +45,6 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="robots" content="noindex, nofollow" />
-          <title>Skatepark.guide - Find skateparks around the world</title>
           <meta
             name="description"
             content="A Progressive Web App for skateparks around the world"
