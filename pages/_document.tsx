fa6427084ae932342dc754c22e026f6a1499d25a
Nico Martin <mail@nicomartin.ch>
2022-02-14 23:54:18 +0100

    added testapp

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,13 +44,6 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="shortcut icon" href="/favicon.ico" />
           <meta name="robots" content="noindex, nofollow" />
-          <title key="title">
-            Skatepark.guide - Find skateparks around the world
-          </title>
-          <meta
-            name="description"
-            content="A Progressive Web App for skateparks around the world"
-          />
           <link
             rel="apple-touch-icon"
             sizes="57x57"
