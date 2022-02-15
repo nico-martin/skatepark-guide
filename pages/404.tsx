0ceb920caf382775581e1b1664f73d303036268d
Nico Martin <mail@nicomartin.ch>
2022-02-15 23:06:51 +0100

    404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useIntl } from 'react-intl';
 import type { NextPage } from 'next';
 import cn from '@common/utils/classnames';
 import contentStyles from './content.module.css';
+import styles from './page.module.css';
 
 const Custom404: NextPage<{ className?: string }> = ({ className = '' }) => {
+  const { formatMessage } = useIntl();
   return (
     <div className={cn(className, contentStyles.root)}>
-      <h1>404 - Page Not Found</h1>
+      <h1 className={styles.title}>{formatMessage({ id: '404.title' })}</h1>
+      <div className={styles.content}>
+        <p>{formatMessage({ id: '404.desc' })}</p>
+      </div>
     </div>
   );
 };
